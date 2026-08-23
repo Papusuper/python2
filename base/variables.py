51b335c53763d7dd8eb6c22faa4755cc82397ffc
Papusuper <samuelbuitrago13@gmail.com>
2026-08-22 19:14:14 -0500

    primera clase

---
+++ base/variables.py
@@ -0,0 +1,30 @@
+#snake_case
+#mi_variable 
+
+mi_texto  = "hola mundo" 
+mi_texto2 = "hola universo"
+mi_texto3 = mi_texto2
+mi_numero = 18
+mi_float = 2,33
+mi_booleano = True
+mi_texto_h  = mi_texto[0:4] 
+
+print(mi_texto)
+print(mi_texto_h)
+print(mi_numero)
+print(mi_float)
+print(mi_booleano)
+print(type(mi_texto))
+
+mi_texto = 7
+print(type(mi_texto))
+
+print(mi_texto)
+mi_texto3 =("hola galaxia")
+print(mi_texto3)
+print(mi_texto2)
+print(mi_texto==mi_texto2)
+print(1+2)
+print(mi_texto2+mi_texto3)
+print("samuel"*18)
+
