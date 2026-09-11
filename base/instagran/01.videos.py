038c98ca3707f1166598ca054c8305e14121b0e0
Papusuper <samuelbuitrago13@gmail.com>
2026-09-10 20:11:18 -0500

    prueba de videos de insta

---
+++ base/instagran/01.videos.py
@@ -0,0 +1,13 @@
+# video 1
+#revisa los diplicados y solo imprime eso 
+x = {1,2,3,}
+y = {3,4,5,2}
+
+print(x & y)
+
+
+
+#video 2
+# tira un error ya que el nuemro no se esta declaranod correctamnete el en print  
+x = 6
+print(x + "6")
